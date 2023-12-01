3fc53cebc2a1e138b868cffd2c31cc38d0756ceb
mrizki-22 <mrizkibgr22@gmail.com>
2023-12-01 09:34:14 +0700

    feat: create maps view button & add post button

--- app/src/main/java/com/foodwaste/mubazir/presentation/browse/BrowseScreen.kt
+++ app/src/main/java/com/foodwaste/mubazir/presentation/browse/BrowseScreen.kt
@@ -1,8 +1,20 @@
 package com.foodwaste.mubazir.presentation.browse
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Map
+import androidx.compose.material.icons.filled.PostAdd
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
@@ -12,8 +24,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.foodwaste.mubazir.R
 import com.foodwaste.mubazir.presentation.browse.component.FilterBottomSheet
 import com.foodwaste.mubazir.presentation.browse.component.FilterRadiusOption
 import com.foodwaste.mubazir.presentation.browse.component.SearchBar
@@ -57,6 +74,38 @@ fun BrowseScreen(
                     openBottomSheet = !openBottomSheet
                 }
             )
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(0.dp, 10.dp)
+            ) {
+                Button(
+                    onClick = {
+                        navController.navigate("maps")
+                    },
+                    shape = RoundedCornerShape(30.dp),
+                    modifier = Modifier.weight(1f)
+                ) {
+                    Icon(imageVector = Icons.Filled.Map, contentDescription = "Maps View")
+                    Spacer(modifier = Modifier.width(10.dp))
+                    Text(text = stringResource(id = R.string.text_maps_view))
+                }
+
+                Spacer(modifier = Modifier.width(10.dp))
+
+                Button(
+                    onClick = {
+                        navController.navigate("add_post")
+                    },
+                    shape = RoundedCornerShape(30.dp),
+                    modifier = Modifier.weight(1f)
+                ) {
+                    Icon(imageVector = Icons.Filled.PostAdd, contentDescription = "Add post")
+                    Spacer(modifier = Modifier.width(10.dp))
+                    Text(text = stringResource(id = R.string.text_add_post))
+                }
+            }
         }
     }
 
@@ -94,4 +143,8 @@ fun BrowseScreen(
     }
 }
 
-
+@Preview
+@Composable
+fun BrowseScreenPreview() {
+    BrowseScreen(navController = rememberNavController())
+}
