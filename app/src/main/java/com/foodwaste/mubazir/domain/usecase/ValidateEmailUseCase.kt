ad31e286017b546b01fea995797f106fab800ddd
mrizki-22 <mrizkibgr22@gmail.com>
2023-12-03 04:57:56 +0700

    feat: create validate usecase

---
+++ app/src/main/java/com/foodwaste/mubazir/domain/usecase/ValidateEmailUseCase.kt
@@ -0,0 +1,22 @@
+package com.foodwaste.mubazir.domain.usecase
+
+import androidx.annotation.StringRes
+import androidx.core.util.PatternsCompat
+
+class ValidateEmailUseCase(
+    /**
+     * [Int] reference to string resource.
+     * You can use string resource to inject these values
+     * */
+    @StringRes private val errorBlankEmail: Int,
+    @StringRes private val errorIncorrectMessage: Int
+) {
+
+    operator fun invoke(email: String): Int? {
+        if (email.isBlank()) return errorBlankEmail
+
+        if (!PatternsCompat.EMAIL_ADDRESS.matcher(email).matches()) return errorIncorrectMessage
+
+        return null
+    }
+}
---
+++ app/src/main/java/com/foodwaste/mubazir/domain/usecase/ValidatePasswordUseCase.kt
@@ -0,0 +1,30 @@
+package com.foodwaste.mubazir.domain.usecase
+
+import androidx.annotation.StringRes
+
+class ValidatePasswordUseCase(
+    /**
+     * [Int] reference to string resource.
+     * You can use string resource to inject these values
+     * */
+    @StringRes private val errorBlankMessage: Int,
+    @StringRes private val errorMinMessage: Int,
+    @StringRes private val errorMaxMessage: Int,
+    @StringRes private val errorNotMatch: Int
+) {
+
+    operator fun invoke(password: String): Int? {
+        if (password.isBlank()) return errorBlankMessage
+
+        if (password.length < 8) return errorMinMessage
+
+        if (password.length > 25) return errorMaxMessage
+
+        return null
+    }
+
+    operator fun invoke(password: String, confirm: String): Int? {
+        if (password != confirm) return errorNotMatch
+        return null
+    }
+}
